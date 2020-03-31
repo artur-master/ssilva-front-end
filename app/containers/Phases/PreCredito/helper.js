47612d97b7a94e0d819170b99d623df0de6cf63b
Artur Shayakhmetov <arturshayakhmetov4@gmail.com>
2020-04-01 04:48:12 +1000

    precredito pago de page fixed error

--- app/containers/Phases/PreCredito/helper.js
+++ app/containers/Phases/PreCredito/helper.js
@@ -32,19 +32,19 @@ export const calculateRenta = (reserva = {}) => {
   if (!Extra.Values) Extra.Values = {};
 
   const Renta = 
-    (Extra.Values.LiquidIncome || 0) +
-    (Extra.Values.VariableSalary || 0) +
-    (Extra.Values.Honoraries || 0) +
-    (Extra.Values.RealStateLeasing || 0) +
-    (Extra.Values.Retirements || 0) +
-    (Extra.Values.Pension || 0);
+    (parseInt(Extra.Values.LiquidIncome) || 0) +
+    (parseInt(Extra.Values.VariableSalary) || 0) +
+    (parseInt(Extra.Values.Honoraries) || 0) +
+    (parseInt(Extra.Values.RealStateLeasing) || 0) +
+    (parseInt(Extra.Values.Retirements) || 0) +
+    (parseInt(Extra.Values.Pension) || 0);
   const CoRenta =
-    (CoExtra.Values.LiquidIncome || 0) +
-    (CoExtra.Values.VariableSalary || 0) +
-    (CoExtra.Values.Honoraries || 0) +
-    (CoExtra.Values.RealStateLeasing || 0) +
-    (CoExtra.Values.Retirements || 0) +
-    (CoExtra.Values.Pension || 0);
+    (parseInt(CoExtra.Values.LiquidIncome) || 0) +
+    (parseInt(CoExtra.Values.VariableSalary) || 0) +
+    (parseInt(CoExtra.Values.Honoraries) || 0) +
+    (parseInt(CoExtra.Values.RealStateLeasing) || 0) +
+    (parseInt(CoExtra.Values.Retirements) || 0) +
+    (parseInt(CoExtra.Values.Pension) || 0);
   const SumRenta = Renta + CoRenta;
   const { total, discount } = calculates(reserva);
   const moneyErr = Math.floor(total - discount) >= SumRenta;
